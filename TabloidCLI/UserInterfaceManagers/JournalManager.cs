30fc4c7be314c2fe7430d2ee85a518fec7cc47b7
Sue Xiong <sxiong0519@gmail.com>
2021-09-14 13:40:25 -0500

    Added get all and insert method for journal.

---
+++ TabloidCLI/UserInterfaceManagers/JournalManager.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TabloidCLI.Models;
+using TabloidCLI.Repositories;
+
+namespace TabloidCLI.UserInterfaceManagers
+{
+    public class JournalManager : IUserInterfaceManager
+    {
+        private readonly IUserInterfaceManager _parentUI;
+        private JournalRepository _journalRepository;
+        private string _connectionString;
+
+        public JournalManager(IUserInterfaceManager parentUI, string connectionString)
+        {
+            _parentUI = parentUI;
+            _journalRepository = new JournalRepository(connectionString);
+            _connectionString = connectionString;
+        }
+
+        public IUserInterfaceManager Execute()
+        {
+            Console.WriteLine("Journal Menu");
+            Console.WriteLine(" 1) List all Journals");
+            Console.WriteLine(" 2) Add Journal");
+/*          Console.WriteLine(" 3) Add Author");
+            Console.WriteLine(" 4) Edit Author");
+            Console.WriteLine(" 5) Remove Author");*/
+            Console.WriteLine(" 0) Go Back");
+
+            Console.Write("> ");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    List();
+                    return this;
+                case "2":
+                    Add();
+                    return this;
+                /*case "3":
+                    Add();
+                    return this;
+                case "4":
+                    Edit();
+                    return this;
+                case "5":
+                    Remove();
+                    return this;*/
+                case "0":
+                    return _parentUI;
+                default:
+                    Console.WriteLine("Invalid Selection");
+                    return this;
+            }
+        }
+
+        private void List()
+        {
+            List<Journal> journals = _journalRepository.GetAll();
+            foreach (Journal journal in journals)
+            {
+                Console.WriteLine($"{journal.Title}");
+                Console.WriteLine($"{journal.Content}");
+                Console.WriteLine($"{journal.CreateDateTime}");
+            }
+        }
+
+        private void Add()
+        {
+            Console.WriteLine("New Journal Entry");
+            Journal journal = new Journal();
+
+            Console.Write("Title:");
+            journal.Title = Console.ReadLine();
+
+            Console.WriteLine("Content:");
+            journal.Content = Console.ReadLine();
+
+            journal.CreateDateTime = DateTime.UtcNow;
+
+            _journalRepository.Insert(journal);
+        }
+    }
+}
